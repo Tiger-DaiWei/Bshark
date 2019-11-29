7d31a3290df79f8e3283cd54226296d94eac80b3
zhang_qi_tai@163.com <Tiger>
2019-11-29 16:07:02 +0800

    fix: 新增页面分页组件

---
+++ src/mixins/index.ts
@@ -0,0 +1,5 @@
+import pager from './pager';
+
+export default {
+  pager,
+}
---
+++ src/mixins/pager.ts
@@ -0,0 +1,33 @@
+import { Component, Vue } from 'vue-property-decorator';
+
+interface Pager {
+  currentPage: number,
+  pageSize: number,
+  pageTotal: number,
+  total: number,
+}
+
+@Component
+class Pagination extends Vue {
+  public pager!: Pager;
+
+  public data() {
+    return {
+      pager: {
+        currentPage: 1,
+        pageSize: 10,
+        pageTotal: 0,
+        total: 0,
+      },
+    };
+  }
+
+  public pageChange(val: number): void {
+    this.pager.currentPage = val;
+    this.$nextTick(() => {
+      (this as any).fetch();
+    });
+  }
+}
+
+export default Pagination;
