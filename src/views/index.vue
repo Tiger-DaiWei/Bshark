8d7979baa92b4fccb26c36a1d41a2faf44728c2a
zhang_qi_tai@163.com <Tiger>
2020-03-31 15:31:20 +0800

    fix: ..

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
     <h1>This is an home page</h1>
-    <Lunbo/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import Lunbo from './../components/lunbo.vue';
 
 @Component({
   components: {
-    Lunbo,
   },
 })
 export default class Home extends Vue {
