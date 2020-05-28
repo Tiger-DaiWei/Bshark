c6c5ac77bc633b231df706ac59f75d6e464e7f6b
zhang_qi_tai@163.com <Tiger>
2020-05-28 12:04:48 +0800

    ..

--- src/views/Portal/index.vue
+++ src/views/Portal/index.vue
@@ -1,77 +1,58 @@
 <template>
   <div class="portal-index">
-    <ul
-      class="content-detail"
-      v-show="changeShow">
-      <li
-        v-for="(item, index) in descriptList"
-        :key="index">
-        <dl>
-          <dt>
-            {{ item.dt }}
-          </dt>
-          <dd>
-            <p
-              v-for="(item1, index) in item.dds"
-              :key="index">
-              {{ item.dds.length > 1 ? index + 1 + '.' : '' }}{{ item1 }}
-            </p>
-          </dd>
-        </dl>
-      </li>
-    </ul>
-    <div
-      class="changeShow"
-      @click="changeShow = !changeShow">
-      <i :class="changeShow ? 'el-icon-arrow-up' : 'el-icon-arrow-down'"></i>
-    </div>
-    <div class="content-suggest">
-      <h4>策略建议:</h4>
-      <p>重点关注家用新车工作日平台异常</p>
-    </div>
-    <!-- 分板块 -->
-    <div class="platform-fluctuation">
-      <h4>1、分板块新车日平台波动情况</h4>
-      <div class="detail">
-        <dl>
-          <dt>前一工作日家用新车平台<span>126.2万</span></dt>
-          <dd>
-            <p>1、前一工作日家用新车平台<span>126.2万</span></p>
-            <p>2、前一工作日家用新车平台<span>126.2万</span></p>
-          </dd>
-        </dl>
-        <div
-          class="image-position"
-          :style="{height: `${numberTotal * 104}px`}">
-          <ImageMap :showObj="showObj"/>
-        </div>
-      </div>
-    </div>
-    <!-- 近期 -->
-    <div class="platform-fluctuation">
-      <h4>2、分板块新车日平台波动情况</h4>
-      <div class="detail">
-        <dl>
-          <dt>前一工作日家用新车平台<span>126.2万</span></dt>
-          <dd>
-            <p>1、前一工作日家用新车平台<span>126.2万</span></p>
-            <p>2、前一工作日家用新车平台<span>126.2万</span></p>
-          </dd>
-        </dl>
-        <div class="image-position">
-        </div>
-      </div>
-    </div>
+  <el-table
+    :data="tableData"
+    height="360"
+    style="width: 100%">
+    <el-table-column
+      prop="data1"
+      label="预警纬度"
+      width="100">
+    </el-table-column>
+    <el-table-column
+      prop="data2"
+      label="规则集"
+      width="100">
+      <template slot-scope="scope">
+        <el-checkbox>{{ scope.row.data2 }}</el-checkbox>
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="data3"
+      label="问题描述">
+      <template slot-scope="scope">
+        <el-input v-model="scope.row.data3" placeholder="请输入内容"></el-input>
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="data4"
+      label="预警时间">
+      <template slot-scope="scope">
+        <el-select v-model="scope.row.data4" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+        <i
+          class="el-icon-circle-plus-outline btn-class"
+          @click="add(scope.row)"></i>
+        <i
+          class="el-icon-circle-close btn-class"
+          @click="del(scope.$index, tableData)"></i>
+      </template>
+    </el-table-column>
+  </el-table>
   </div>
 </template>
 
 <script lang='ts'>
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import ImageMap from '@/components/imageMap.vue';
 
 @Component({
   components: {
-    ImageMap,
   },
 })
 export default class Portal extends Vue {
@@ -79,231 +60,81 @@ export default class Portal extends Vue {
   public showObj!: any[];
   public numberTotal!: number;
   public descriptList!: any[];
+  public tableData!: any[];
+  public options!: any[];
   public data(): any {
     return {
-      someData: [1, [2, 3], [4, [5, 6], 7], 8],
-      showObj: [{
-        title: '新车较市场',
-        list: ['增速：10.2%', '较市场：-0.8pt'],
-        styleList: 'color1',
-        children: [
-          {
-            title: '家用新车较市场',
-            list: ['增速：10.2%', '较市场：-0.8pt'],
-            styleList: 'color1',
-            children: [
-              {
-                title: '家用新4s较市场',
-                list: ['增速：10.2%', '较市场：-0.8pt'],
-                styleList: 'color1',
-                children: [
-                  {
-                    title: '家用新4s费用环比滚动3个月',
-                    list: ['增速：10.2%', '较市场：-0.8pt'],
-                    styleList: 'color1',
-                    children: [
-                    ],
-                  },
-                  {
-                    title: '家用新4s费用环比家用续',
-                    list: ['增速：10.2%', '较市场：-0.8pt'],
-                    styleList: 'color2',
-                    children: [
-                    ],
-                  },
-                ],
-              },
-              {
-                title: '家用新非4s较市场',
-                list: ['增速：10.2%', '较市场：-0.8pt'],
-                styleList: 'color1',
-                children: [
-                  {
-                    title: '家用新非4s费用环比滚动3个月',
-                    list: ['增速：10.2%', '较市场：-0.8pt'],
-                    styleList: 'color2',
-                    children: [
-                    ],
-                  },
-                  {
-                    title: '家用新4s费用环比家用续',
-                    list: ['增速：10.2%', '较市场：-0.8pt'],
-                    styleList: 'color2',
-                    children: [
-                    ],
-                  },
-                  {
-                    title: '家用新4s费用环比家用续',
-                    list: ['增速：10.2%', '较市场：-0.8pt'],
-                    styleList: 'color2',
-                    children: [
-                  {
-                    title: '家用新4s费用环比家用续',
-                    list: ['增速：10.2%', '较市场：-0.8pt'],
-                    styleList: 'color2',
-                    children: [
-                    ],
-                  },
-                  {
-                    title: '家用新4s费用环比家用续',
-                    list: ['增速：10.2%', '较市场：-0.8pt'],
-                    styleList: 'color2',
-                    children: [
-                    ],
-                  },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            title: '商用新车',
-            list: ['增速：10.2%', '较市场：-0.8pt'],
-            styleList: 'color2',
-            children: [
-              {
-                title: '商用新车',
-                list: ['增速：10.2%', '较市场：-0.8pt'],
-                styleList: 'color2',
-                children: [
-
-              {
-                title: '商用新车',
-                list: ['增速：10.2%', '较市场：-0.8pt'],
-                styleList: 'color2',
-                children: [],
-              },
-                ],
-              },
-              {
-                title: '商用新车',
-                list: ['增速：10.2%', '较市场：-0.8pt'],
-                styleList: 'color2',
-                children: [],
-              },
-            ],
-          },
-          {
-            title: '商用新车1',
-            list: ['增速：10.2%', '较市场：-0.8pt'],
-            styleList: 'color2',
-            children: [],
-          },
-          {
-            title: '商用新车2',
-            list: ['增速：10.2%', '较市场：-0.8pt'],
-            styleList: 'color2',
-            children: [],
-          },
-        ],
+      tableData: [{
+        data1: '整体',
+        data2: '规则',
+        data3: '当月企划',
+        data4: '全月',
       }],
-      numberTotal: 0,
-      descriptList: [
+      options: [
         {
-          dt: '问题',
-          dds: [
-            '预估全月落后市场',
-          ],
+          value: 'A01',
+          label: '全日',
         },
         {
-          dt: '原因',
-          dds: [
-            '预估全月落后市场',
-            '预估全月落后市场',
-            '预估全月落后市场',
-          ],
-        },
-        {
-          dt: '举措',
-          dds: [
-            '预估全月落后市场',
-            '预估全月落后市场',
-            '预估全月落后市场',
-          ],
-        },
-        {
-          dt: '目标',
-          dds: [
-            '预估全月落后市场',
-            '预估全月落后市场',
-            '预估全月落后市场',
-          ],
-        },
-        {
-          dt: '责任人',
-          dds: [
-            '预估全月落后市场',
-            '预估全月落后市场',
-            '预估全月落后市场',
-          ],
-        },
-        {
-          dt: '时间',
-          dds: [
-            '预估全月落后市场',
-            '预估全月落后市场',
-            '预估全月落预估预估全月落后市场预估全月落后市场全月落后市场后市场',
-          ],
+          value: 'A03',
+          label: '全月',
         },
       ],
-      changeShow: true,
     };
   }
 
   public beforeCreate() {
-    console.log('beforeCreate');
-    console.log(this.$el);
-    console.log(this.$data);
+    // console.log('beforeCreate');
+    // console.log(this.$el);
+    // console.log(this.$data);
   }
 
   public created() {
-    console.log('created');
-    console.log(this.$el);
-    console.log(this.$data);
+    // console.log('created');
+    // console.log(this.$el);
+    // console.log(this.$data);
   }
 
   public beforeMount() {
-    console.log('beforeMount');
-    console.log(this.$el);
-    console.log(this.$data);
+    // console.log('beforeMount');
+    // console.log(this.$el);
+    // console.log(this.$data);
   }
 
   public mounted() {
-    this.numberTotal = this.$utils.getTotal(this.showObj);
-    console.log(this.numberTotal);
-    console.log(this.$el);
-    console.log(this.$data);
+    // console.log(this.numberTotal);
+    // console.log(this.$el);
+    // console.log(this.$data);
   }
 
   public method() {
-    console.log('mothod');
-    console.log(this.$el);
-    console.log(this.$data);
+    // console.log('mothod');
+    // console.log(this.$el);
+    // console.log(this.$data);
   }
 
   public beforeUpdate() {
-    console.log('beforeUpdate');
-    console.log(this.$el);
-    console.log(this.$data);
+    // console.log('beforeUpdate');
+    // console.log(this.$el);
+    // console.log(this.$data);
   }
 
   public updated() {
-    console.log('update');
-    console.log(this.$el);
-    console.log(this.$data);
+    // console.log('update');
+    // console.log(this.$el);
+    // console.log(this.$data);
   }
 
   public beforeDestroy() {
-    console.log('beforeDestory');
-    console.log(this.$el); // el元素的挂载节点
-    console.log(this.$data); // data属性
+    // console.log('beforeDestory');
+    // console.log(this.$el); // el元素的挂载节点
+    // console.log(this.$data); // data属性
   }
 
   public destroyed() {
-    console.log('destroyed');
-    console.log(this.$el);
-    console.log(this.$data);
+    // console.log('destroyed');
+    // console.log(this.$el);
+    // console.log(this.$data);
   }
 
   // 将多维数组转为一维数组
@@ -318,146 +149,22 @@ export default class Portal extends Vue {
     });
     return newArray;
   }
+
+  public add(obj: object) {
+    this.tableData.push(obj);
+  }
+
+  public del(num: number, arrs: any[]) {
+    arrs.splice(num, 1);
+  }
+
 }
 </script>
 
 <style lang="less" scoped>
 .portal-index {
-  padding: 50px;
-  // 列表描述问题
-  .content-detail {
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    border-radius: 4px;
-    border: 1px solid #e8e8e8;
-    li {
-      flex: 1;
-      border-left: 1px solid #e8e8e8;
-      margin-left: -1px;
-      color: #333;
-      dl {
-        height: 100%;
-        dt {
-          background: #F6F6F6;
-          line-height: 24px;
-          padding: 8px 20px;
-          font-size: 16px;
-          font-weight: 700;
-        }
-        dd {
-          padding: 20px;
-          height: calc(~'100% - 80px');
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          p {
-            line-height: 24px;
-            font-size: 14px;
-          }
-        }
-      }
-    }
-  }
-  // 展开收起
-  .changeShow {
-    color: #4D7AFF;
-    width: 14px;
-    height: 12px;
-    margin: 0 auto;
-    padding: 10px;
-    cursor: pointer;
-    i {
-      width: 14px;
-      font-weight: 700;
-    }
-  }
-  // 策略建议
-  .content-suggest {
-    border-radius: 4px;
-    border: 1px solid #e8e8e8;
-    height: 52px;
-    color: #333;
-    padding: 13px 30px;
-    box-sizing: border-box;
-    margin-bottom: 20px;
-    h4, p {
-      line-height: 24px;
-      height: 24px;
-      display: inline-block;
-    }
-    h4 {
-      font-size: 16px;
-      font-weight: 700;
-      margin-right: 8px;
-      position: relative;
-      &:after {
-        display: block;
-        content: '';
-        width: 4px;
-        height: 4px;
-        border-radius: 50%;
-        background-color: #999;
-        position: absolute;
-        left: -11px;
-        top: 10px;
-      }
-    }
-  }
-  // 平台波动
-  .platform-fluctuation {
-    border: 1px solid #DEE6FF;
-    background-color: #fff;
-    border-radius: 4px;
-    font-size: 14px;
-    margin-bottom: 20px;
-    h4 {
-      background: rgba(237,241,255,0.22);
-      border-bottom: 1px solid #DEE6FF;
-      line-height: 24px;
-      padding: 8px 20px;
-      color: #333;
-    }
-    .detail {
-      display: flex;
-      flex-direction: row;
-      dl {
-        width: 550px;
-        padding: 36px 20px;
-        box-sizing: border-box;
-        dt {
-          line-height: 24px;
-          padding-left: 10px;
-          position: relative;
-          &:before {
-            display: block;
-            content: '';
-            width: 3px;
-            height: 10px;
-            background: #4D7AFF;
-            position: absolute;
-            top: 7px;
-            left: 0px;
-          }
-          span {
-            color: #4D7AFF;
-          }
-        }
-        dd {
-          margin-top: 18px;
-          p {
-            line-height: 30px;
-          }
-        }
-      }
-      // 图谱样式
-      div.image-position {
-        flex: 1;
-        overflow: auto;
-        position: relative;
-        border-left: 1px dashed #ccc;
-      }
-    }
+  i.btn-class {
+    font-size: 20px;
   }
 }
 </style>
