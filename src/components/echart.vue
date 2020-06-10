ec8089a06c78e4e7cec5a49222a243c3bac75ef7
zhang_qi_tai@163.com <Tiger>
2020-06-10 11:47:33 +0800

    ..

--- src/components/echart.vue
+++ src/components/echart.vue
@@ -5,36 +5,21 @@
 </template>
 
 <script lang='ts'>
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Mixins, Vue } from 'vue-property-decorator';
+import EchartMixins from '../mixins/echart';
 
 @Component({
   components: {},
 })
 
-export default class Echart extends Vue {
+export default class Echart extends Mixins(EchartMixins) {
   public $echarts: any;
-  private options: object = {
-    // 里面写要显示的echarts图标
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
-      },
-    ],
-  };
 
   private mounted() {
     const ele = document.getElementById('myEcharts');
     const chart: any = this.$echarts.init(ele);
     // 调接口获取数据
-    chart.setOption(this.options);
+    chart.setOption(this.echartsData);
   }
 }
 </script>
---
+++ src/mixins/echart.ts
@@ -0,0 +1,45 @@
+import { Component, Vue } from 'vue-property-decorator';
+
+interface EchartsData {
+  title: object; // 标题组件，包含主标题和副标题
+  legend: object; // 图例组件
+  xAxis: object;
+  yAxis: object;
+  series: any[];
+}
+
+@Component
+class EchartMixins extends Vue {
+  public echartsData!: EchartsData;
+
+  public data() {
+    return {
+      echartsData: {
+        title: {
+          show: true,
+          text: '趋势图测试',
+        },
+        legend: {},
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        },
+        yAxis: {
+          type: 'value',
+        },
+        series: [
+          {
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: 'line',
+          },
+          {
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: 'bar',
+          },
+        ],
+      },
+    };
+  }
+}
+
+export default EchartMixins;
